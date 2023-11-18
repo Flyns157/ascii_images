1dbeb9b2a68d6c9746f98ab533212f5670028640
Mattéo Cuisset <matteo.cuisset@gmail.com>
2023-11-18 21:46:50 +0100

    Update of the Main file (now in V2.0)

--- Main.py
+++ Main.py
@@ -2,28 +2,49 @@
 from PIL import Image
 
 # ================================================ Init =================================================
-VERSION = 1.0
-# Cleaning output file
-tmp = open('out.txt','w')
-tmp.write('')
-tmp.close()
-# Some initializations
-file = open('out.txt','a')
-size = 100
-# gradations from darkest to lightest
-gscale1 = ' .:-=+*#%@' # 10 levels of gray
-gscale2 = ''' ."`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$''' #~70 levels of gray
-ascii_char = gscale1
+VERSION = 2.0
+NAME = __file__
 
-# =============================================== Program ===============================================
-with Image.open("logo.ico") as image:
-    image = image.resize((size, size))
+# ============================================== Functions ==============================================
+def image_to_ascii_art(input_file : str, outpout_file : str = 'out.txt', resize : bool = None, resize_percentage : float = 50, xsize : int = 50, ysize : int = 50, gscale : int = 0, nb_space : int = 1)-> bool :
+    """
+    Converts an image to ASCII art and writes it to a file.
 
-    for y in range(image.height):
-        line = ""
-        for x in range(image.width):
-            line += ascii_char[(sum(image.getpixel((x, y))) // len(image.getpixel((x, y)))) * (len(ascii_char) - 1) // 255] + ' '
-        file.write(line + '\n')
+    Parameters:
+        input_file (str): The path to the input image file.
+        outpout_file (str, optional): The path to the output text file. Defaults to 'out.txt'.
+        resize (bool, optional): Whether to resize the image or not. If None, the image is resized to (xsize, ysize). If True, the image is resized by resize_percentage. Defaults to None.
+        resize_percentage (float, optional): The percentage to resize the image by if resize is True. Defaults to 50.
+        xsize (int, optional): The width to resize the image to if resize is None. Defaults to 50.
+        ysize (int, optional): The height to resize the image to if resize is None. Defaults to 50.
+        gscale (int, optional): The grayscale level to use for the ASCII art. 0 for 10 levels of gray, 1 for ~70 levels of gray. Defaults to 0.
+        nb_space (int, optional): The number of spaces to add between ASCII characters. Defaults to 1.
 
-# =============================================== closing ===============================================
-file.close()
+    Returns:
+        bool: True if the ASCII art was successfully written to the file, False otherwise.
+    """
+    try :
+        # Clear output file
+        tmp = open(outpout_file,'w')
+        tmp.write('')
+        tmp.close()
+        # Create the output
+        file = open(outpout_file,'a')
+        ascii_char = [' .:-=+*#%@',''' ."`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'''][gscale] #10 levels of gray then ~70 levels of gray
+        with Image.open(input_file) as image:
+            if resize == None :
+                image = image.resize((xsize, ysize))
+            elif resize :
+                image = image.resize((image.width//resize_percentage, image.height//resize_percentage))
+            # Create the ascii image
+            file.write(''.join(''.join((ascii_char[(sum(image.getpixel((x, y))) // len(image.getpixel((x, y)))) * (len(ascii_char) - 1) // 255] + ' '*nb_space for x in range(image.width))) + '\n' for y in range(image.height)))
+    except Exception as e:
+        print(f'caught {type(e)}: {e}')
+    finally :
+        try :
+            file.close()
+        except Exception :
+            pass
+
+# ================================================= Run =================================================
+image_to_ascii_art("images/2.jpg", resize = False, nb_space = 1)
